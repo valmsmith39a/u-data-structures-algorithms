04bf9de839f5b22f7b42e10a7e754d3e2db2bfe9
valmsmith39a <valmsmith39a@gmail.com>
2020-02-17 14:41:50 -0800

    iscircular

--- linked-lists.py
+++ linked-lists.py
@@ -328,3 +328,20 @@ def reverse(linked_list):
     new_list.head = prev_node
 
     return new_list
+
+def iscircular(linked_list):
+  if linked_list.head is None:
+        return False
+
+  slow_runner = linked_list.head
+  fast_runner = linked_list.head
+
+  while fast_runner.next:
+      slow_runner = slow_runner.next
+      fast_runner = fast_runner.next.next
+
+      if slow_runner == fast_runner:
+          return True
+
+  return False
+  
