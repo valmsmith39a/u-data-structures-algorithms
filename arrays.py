e3dfb5ab149e23e7e91baabf2eb979b4f9b01404
valmsmith39a <valmsmith39a@gmail.com>
2020-02-23 18:11:12 -0800

    skip i delete j

--- arrays.py
+++ arrays.py
@@ -139,3 +139,36 @@ def nth_row_pascal(n):
         return even
     odd_tail.next = even
     return odd
+
+# skip i delete j
+def skip_i_delete_j(head, i, j):
+    """
+    :param: head - head of linked list
+    :param: i - first `i` nodes that are to be skipped
+    :param: j - next `j` nodes that are to be deleted
+    return - return the updated head of the linked list
+    """
+    if i == 0:
+        return None
+    
+    if head is None or j < 0 or i < 0:
+        return head
+    
+    current = head
+    previous = None
+    
+    while current:
+        for _ in range(i-1):
+            if current == None:
+                return head
+            current = current.next
+        previous = current
+        current = current.next
+        
+        for _ in range (j):
+            if current is None:
+                break
+            next_node = current.next
+            current = next_node
+        previous.next = current
+    return head
