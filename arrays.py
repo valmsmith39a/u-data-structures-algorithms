c1a9025a0e8094580924311928d7be6a7c5148c0
valmsmith39a <valmsmith39a@gmail.com>
2020-02-23 17:34:51 -0800

    even after odd

--- arrays.py
+++ arrays.py
@@ -99,3 +99,43 @@ def nth_row_pascal(n):
           current_row.append(next_number)
       current_row.append(1)
   return current_row
+
+  # even after odd
+  def even_after_odd(head):
+    """
+    :param - head - head of linked list
+    return - updated list with all even elements are odd elements
+    """
+    even = None
+    even_tail = None
+    odd = None
+    odd_tail = None
+    
+    if head is None:
+        return head
+    
+    while head:
+        next_node = head.next
+        
+        if head.data % 2 == 0:
+            if even is None:
+                even = head
+                even_tail = even
+            else:
+                even_tail.next = head
+                even_tail = even_tail.next
+        else: 
+            if odd is None:
+                odd = head
+                odd_tail = odd
+            else:
+                odd_tail.next = head
+                odd_tail = odd_tail.next
+                
+        head.next = None
+        head = next_node
+        
+    if odd is None:
+        return even
+    odd_tail.next = even
+    return odd
