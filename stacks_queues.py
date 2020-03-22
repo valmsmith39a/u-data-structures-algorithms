c0acdb0b6366371271581e661b71faa1dd70b0df
valmsmith39a <valmsmith39a@gmail.com>
2020-03-21 19:58:13 -0700

    push to stack

--- stacks_queues.py
+++ stacks_queues.py
@@ -5,8 +5,22 @@ class Stack:
     self.next_index = 0
     self.num_elements = 0
 
+  def push(self, data):
+    self.arr[self.next_index] = data
+    self.next_index += 1 
+    self.num_elements += 1
+
+"""
+# Test creating the Stack
 foo = Stack()
 print('Pass' if foo.arr == [0, 0, 0, 0, 0, 0, 0, 0, 0, 0] else 'Fail')
+
+# Test pushing to the Stack
+foo = Stack()
+foo.push('Test')
+print('Pass' if foo.arr[0] == 'Test' else 'Fail')
+"""
+
 
 
 
