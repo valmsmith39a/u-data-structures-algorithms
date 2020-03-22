783e057096ab035411e0fb35578bfb155e967f61
valmsmith39a <valmsmith39a@gmail.com>
2020-03-21 19:31:38 -0700

    create a stack object

---
+++ stacks_queues.py
@@ -0,0 +1,12 @@
+class Stack:
+
+  def __init__(self, initial_size = 10):
+    self.arr = [ 0 for _ in range(initial_size)]
+    self.next_index = 0
+    self.num_elements = 0
+
+foo = Stack()
+print('Pass' if foo.arr == [0, 0, 0, 0, 0, 0, 0, 0, 0, 0] else 'Fail')
+
+
+
