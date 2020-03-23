de6fab71d70c1e2d5a73e70d1b5b189c4eb1a459
valmsmith39a <valmsmith39a@gmail.com>
2020-03-22 19:20:58 -0700

    handle stack overflow

--- stacks_queues.py
+++ stacks_queues.py
@@ -6,9 +6,20 @@ class Stack:
     self.num_elements = 0
 
   def push(self, data):
+    if(self.next_index == len(self.arr)):
+      print("Out of space! Increasing array capacity ...")
+      self._handle_stack_capacity_full()
+
     self.arr[self.next_index] = data
     self.next_index += 1 
     self.num_elements += 1
+
+  def _handle_stack_capacity_full(self):        
+        old_arr = self.arr
+        self.arr = [0 for _ in range(2*len(old_arr))]
+        
+        for idx, element in enumerate(old_arr):
+            self.arr[idx] = old_arr[idx]
 
 """
 # Test creating the Stack
@@ -19,7 +30,24 @@ print('Pass' if foo.arr == [0, 0, 0, 0, 0, 0, 0, 0, 0, 0] else 'Fail')
 foo = Stack()
 foo.push('Test')
 print('Pass' if foo.arr[0] == 'Test' else 'Fail')
+
+# Handle stack overflow
+foo = Stack()
+foo.push(1)
+foo.push(2)
+foo.push(3)
+foo.push(4)
+foo.push(5)
+foo.push(6)
+foo.push(7)
+foo.push(8)
+foo.push(9)
+foo.push(10) # Array at capacity
+foo.push(11) # Array increases in size
+print(foo.arr) # new array
+print("Pass" if len(foo.arr) == 20 else "Fail") # Array size should be 20 if doubled the size
 """
+
 
 
 
