2a01b92dbf2adf2f5afd3a16dd5875e2e939bc24
valmsmith39a <valmsmith39a@gmail.com>
2020-03-28 20:07:01 -0700

    pop

--- stacks_queues.py
+++ stacks_queues.py
@@ -14,6 +14,14 @@ class Stack:
     self.next_index += 1 
     self.num_elements += 1
 
+  def pop(self):
+    if self.is_empty():
+      self.next_index = 0
+      return None
+    self.next_index -= 1
+    self.num_elements -= 1
+    return self.arr[self.next_index]
+
   def _handle_stack_capacity_full(self):        
         old_arr = self.arr
         self.arr = [0 for _ in range(2*len(old_arr))]
@@ -26,7 +34,6 @@ class Stack:
 
   def is_empty(self):
     return self.num_elements == 0
-
 
 """
 # Test creating the Stack
@@ -60,4 +67,11 @@ foo.push('test')
 print(foo.is_empty())
 print(foo.size())
 
+# pop
+foo = Stack()
+foo.push('test1')
+foo.push('test2')
+print('Pass' if foo.pop() == 'test2' else 'Fail')
+print('Pass' if foo.pop() == 'test1' else 'Fail')
+print('Pass' if foo.pop() == None else 'Fail')
 """
