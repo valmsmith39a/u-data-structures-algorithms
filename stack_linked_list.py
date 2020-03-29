d206b70d749f3e66488465db0a0753f84be41d0c
valmsmith39a <valmsmith39a@gmail.com>
2020-03-29 16:41:51 -0700

    pop method and tests

--- stack_linked_list.py
+++ stack_linked_list.py
@@ -1,8 +1,8 @@
 class Node:
 
-  def __init__(self, value):
-    self.value = value
-    self.next = None
+    def __init__(self, value):
+        self.value = value
+        self.next = None
 
 
 class Stack:
@@ -22,9 +22,38 @@ class Stack:
 
         self.num_elements += 1
 
+    def pop(self):
+        if self.is_empty():
+            return None
+        value = self.head.value
+        self.head = self.head.next
+        self.num_elements -= 1
+        return value
+
     def size(self):
         return self.num_elements
-    
+
     def is_empty(self):
         return self.num_elements == 0
-        
+
+stack = Stack()
+stack.push(10)
+stack.push(20)
+stack.push(30)
+stack.push(40)
+stack.push(50)
+
+# Test size 
+print('Pass' if stack.size() == 5 else 'Fail')
+
+# Test pop
+print ("Pass" if (stack.pop() == 50) else "Fail")
+
+# Test push
+stack.push(60)
+print ("Pass" if (stack.pop() == 60) else "Fail")
+print ("Pass" if (stack.pop() == 40) else "Fail")
+print ("Pass" if (stack.pop() == 30) else "Fail")
+stack.push(50)
+print ("Pass" if (stack.size() == 3) else "Fail")
+
