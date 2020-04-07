7d56d2c18ae2942f2fb4fcfeabfd931ec51dd762
valmsmith39a <valmsmith39a@gmail.com>
2020-04-06 21:05:17 -0700

    dequeue method

--- queues.py
+++ queues.py
@@ -1,25 +1,34 @@
 class Queue:
-    def __init__(self, initial_size=10):
-        self.arr = [0 for x in range(initial_size)]
-        self.next_index = 0
-        self.front_index = -1
-        self.queue_size = 0
+  def __init__(self, initial_size=10):
+      self.arr = [0 for x in range(initial_size)]
+      self.next_index = 0
+      self.front_index = -1
+      self.queue_size = 0
 
-     def enqueue(self, value):
-        self.arr[self.next_index] = value
-        self.queue_size += 1
-        self.next_index = (self.next_index + 1) % len(self.arr)
-        if self.front_index == -1:
-            self.front_index = 0
+  def enqueue(self, value):
+    self.arr[self.next_index] = value
+    self.queue_size += 1
+    self.next_index = (self.next_index + 1) % len(self.arr)
+    if self.front_index == -1:
+        self.front_index = 0
 
-    def size(self):
-        return self.queue_size
-    
-    def is_empty(self):
-        return size() == 0
+  def dequeue(self):
+      if self.is_empty():
+          self.front_index = -1
+          self.next_index = 0
+          return None
+      value = self.arr[self.front_index]
+      self.front_index = (self.front_index) + 1 % len(self.arr)
+      self.queue_size -= 1 
+      return value
 
-    def front(self):
-        if self.is_empty():
-            return None
-        return self.arr[self.front_index]
-        
+  def size(self):
+      return self.queue_size
+  
+  def is_empty(self):
+      return size() == 0
+
+  def front(self):
+      if self.is_empty():
+          return None
+      return self.arr[self.front_index]
