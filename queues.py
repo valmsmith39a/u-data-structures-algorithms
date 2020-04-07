a97e379d7303679faec401b218c3557a1e3b2c95
valmsmith39a <valmsmith39a@gmail.com>
2020-04-06 20:45:17 -0700

    size, is_empty, front methods

--- queues.py
+++ queues.py
@@ -11,3 +11,15 @@ class Queue:
         self.next_index = (self.next_index + 1) % len(self.arr)
         if self.front_index == -1:
             self.front_index = 0
+
+    def size(self):
+        return self.queue_size
+    
+    def is_empty(self):
+        return size() == 0
+
+    def front(self):
+        if self.is_empty():
+            return None
+        return self.arr[self.front_index]
+        
