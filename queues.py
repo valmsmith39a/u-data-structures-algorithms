99966ddd9311a1ad8c9c04cd6587c1127bb37bd9
valmsmith39a <valmsmith39a@gmail.com>
2020-04-06 20:31:53 -0700

    queue: init method

---
+++ queues.py
@@ -0,0 +1,6 @@
+class Queue:
+    def __init__(self, initial_size=10):
+        self.arr = [0 for x in range(initial_size)]
+        self.next_index = 0
+        self.front_index = -1
+        self.queue_size = 0
