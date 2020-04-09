cc313124e552bbdf0aeb04ff9cdde47e936be307
valmsmith39a <valmsmith39a@gmail.com>
2020-04-08 19:47:38 -0700

    size, is_empty, dequeue

--- queue_linked_list.py
+++ queue_linked_list.py
@@ -1,9 +1,8 @@
 class Node():
 
-    __init__(self, value):
+  def __init__(self, value):
         self.value = value
         self.next = None
-
 
 class Queue:
 
@@ -24,3 +23,40 @@ class Queue:
             self.tail = self.tail.next
 
         self.num_elements += 1
+
+    def size(self):
+        return self.num_elements
+    
+    def is_empty(self):
+        return self.num_elements == 0
+
+    def dequeue(self):
+        if self.head is None:
+            return None
+        value = self.head.value
+        self.head = self.head.next
+        self.num_elements -= 1
+        return value
+
+
+# Tests
+
+# Setup
+q = Queue()
+q.enqueue(1)
+q.enqueue(2)
+q.enqueue(3)
+
+# Test size
+print ("Pass" if (q.size() == 3) else "Fail")
+
+# Test dequeue
+print ("Pass" if (q.dequeue() == 1) else "Fail")
+
+# Test enqueue
+q.enqueue(4)
+print ("Pass" if (q.dequeue() == 2) else "Fail")
+print ("Pass" if (q.dequeue() == 3) else "Fail")
+print ("Pass" if (q.dequeue() == 4) else "Fail")
+q.enqueue(5)
+print ("Pass" if (q.size() == 1) else "Fail")
