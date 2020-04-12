128ce22b6b3da8f9b6bd0416c3f79e8d822b4e55
valmsmith39a <valmsmith39a@gmail.com>
2020-04-11 17:26:27 -0700

    sum array: recursive (slicing, index) and iterative

--- recursion.py
+++ recursion.py
@@ -5,4 +5,35 @@ def sum_integers(n):
     return n + sum_integers(n - 1)
 
 
-print(sum_integers(3))
+print(sum_integers(4))
+
+def sum_array(array):
+    # Base Case
+    if len(array) == 1:
+        return array[0]
+    
+    return array[0] + sum_array(array[1:])
+
+arr = [1, 2, 3, 4]
+print(sum_array(arr))
+
+def sum_array_index(array, index):
+    # Base Cases
+    if len(array) - 1 == index:
+        return array[index]
+    
+    return array[index] + sum_array_index(array, index + 1)
+
+arr = [1, 2, 3, 4]
+print(sum_array_index(arr, 0))
+
+def sum_array_iter(array):
+    result = 0
+    
+    for x in array:
+        result += x
+    
+    return result
+
+arr = [1, 2, 3, 4]
+print(sum_array_iter(arr))
