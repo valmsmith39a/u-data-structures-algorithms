2c4d404c5684a84edbf98d413aadfdb01ad8962f
valmsmith39a <valmsmith39a@gmail.com>
2020-04-11 18:28:10 -0700

    recursion: reverse a string

--- recursion.py
+++ recursion.py
@@ -7,41 +7,48 @@ def sum_integers(n):
 
 print(sum_integers(4))
 
+
 def sum_array(array):
     # Base Case
     if len(array) == 1:
         return array[0]
-    
+
     return array[0] + sum_array(array[1:])
+
 
 arr = [1, 2, 3, 4]
 print(sum_array(arr))
+
 
 def sum_array_index(array, index):
     # Base Cases
     if len(array) - 1 == index:
         return array[index]
-    
+
     return array[index] + sum_array_index(array, index + 1)
+
 
 arr = [1, 2, 3, 4]
 print(sum_array_index(arr, 0))
 
+
 def sum_array_iter(array):
     result = 0
-    
+
     for x in array:
         result += x
-    
+
     return result
+
 
 arr = [1, 2, 3, 4]
 print(sum_array_iter(arr))
 
+
 def factorial(n):
     """
     Calculate n!
-    
+
     Args:
        n(int): factorial to be computed
     Returns:
@@ -59,4 +66,27 @@ def factorial(n):
         return 1
 
     return n * factorial(n-1)
-    
+
+
+def reverse_string(input):
+    """
+    Return reversed input string
+
+    Examples:
+        reverse_string("abc") returns "cba"
+
+    Args:
+      input(str): string to be reversed
+
+    Returns:
+      a string that is the reverse of input
+    """
+
+    if len(input) == 0:
+        return ""
+    return input[-1] + reverse_string(input[0:-1])
+
+
+# Test Cases
+print("Pass" if ("" == reverse_string("")) else "Fail")
+print("Pass" if ("cba" == reverse_string("abc")) else "Fail")
