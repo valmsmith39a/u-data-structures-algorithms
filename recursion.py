4a3dc6edd9336747782b061e530b98b8b4681cf2
valmsmith39a <valmsmith39a@gmail.com>
2020-04-11 17:45:16 -0700

    recursion: factorial

--- recursion.py
+++ recursion.py
@@ -37,3 +37,26 @@ def sum_array_iter(array):
 
 arr = [1, 2, 3, 4]
 print(sum_array_iter(arr))
+
+def factorial(n):
+    """
+    Calculate n!
+    
+    Args:
+       n(int): factorial to be computed
+    Returns:
+       n!
+    """
+    # 5! = 5 * 4!
+    # 4! = 4 * 3!
+    # ...
+    # 2! = 2 * 1!
+    # 1! = 1 * 0!
+    # base case: n = 0, return 1
+    # factorial(5) = 5 * factorial(4)
+    # factorial(4) = 4 * factorial (3)
+    if n == 0:
+        return 1
+
+    return n * factorial(n-1)
+    
