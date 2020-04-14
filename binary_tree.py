b540e57644e883e3d70d7eb927f64acbde45342e
valmsmith39a <valmsmith39a@gmail.com>
2020-04-13 21:32:57 -0700

    define has left/right children and Tree class

--- binary_tree.py
+++ binary_tree.py
@@ -23,13 +23,13 @@ class Node(object):
     def get_right_child(self, node):
         return self.right
 
-node0 = Node()
-print(f"""
-value: {node0.value}
-left: {node0.left}
-right: {node0.right}
-""")
+    def has_left_child(self):
+        return self.left != None
 
+    def has_right_child(self):
+        return self.right != None
+
+# Check 
 node0 = Node()
 print(f"""
 value: {node0.value}
@@ -44,6 +44,7 @@ left: {node0.left}
 right: {node0.right}
 """)
 
+# Check 
 node0 = Node("apple")
 node1 = Node("banana")
 node2 = Node("orange")
@@ -55,3 +56,27 @@ node 0: {node0.value}
 node 0 left child: {node0.left.value}
 node 0 right child: {node0.right.value}
 """)
+
+# Check
+node0 = Node("apple")
+node1 = Node("banana")
+node2 = Node("orange")
+
+print(f"has left child? {node0.has_left_child()}")
+print(f"has right child? {node0.has_right_child()}")
+
+print("adding left and right children")
+node0.set_left_child(node1)
+node0.set_right_child(node2)
+
+print(f"has left child? {node0.has_left_child()}")
+print(f"has right child? {node0.has_right_child()}")
+
+class Tree(object):
+    
+    def __init__(self, value):
+        self.root = Node(value)
+        
+    def get_root(self):
+        return self.root
+        
