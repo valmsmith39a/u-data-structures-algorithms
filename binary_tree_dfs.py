19dcf61f6364396c233667565ed50e3879571ba6
valmsmith39a <valmsmith39a@gmail.com>
2020-04-15 20:01:00 -0700

    pre-order traversal with stack: v1

--- binary_tree_dfs.py
+++ binary_tree_dfs.py
@@ -56,6 +56,7 @@ tree.get_root().get_left_child().set_left_child(Node('dates'))
 # Binary tree traversal depth first, pre-order traversal with a stack
 # Stack using a list
 
+
 class Stack():
 
     def __init__(self):
@@ -65,7 +66,7 @@ class Stack():
         self.list.append(value)
 
     def pop(self):
-        self.list.pop()
+        return self.list.pop()
 
     def top(self):
         if len(self.list) > 0:
@@ -85,6 +86,7 @@ class Stack():
 
         else:
             return "<stack is empty>"
+
 
 # check Stack
 stack = Stack()
@@ -120,8 +122,8 @@ print(f"{node} has left child? {node.has_left_child()}")
 print(node.get_left_child())
 
 if node.has_left_child():
-  node = node.get_left_child()
-  stack.push(node)
+    node = node.get_left_child()
+    stack.push(node)
 
 print(f"""
 visit_order {visit_order} 
@@ -135,4 +137,160 @@ print(f"""visit_order {visit_order}""")
 
 # depth first search, pre-order traversal
 # get the node and push it to the stack
-# visit the node (get_value) and add it to the stack
+# visit the node (get_value) and add it to the visit_order
+
+print(f"{node} has left child? {node.has_left_child()}")
+
+if node.has_left_child():
+    node = node.get_left_child()
+    stack.push(node)
+
+print(f"""
+visit_order {visit_order} 
+stack:
+{stack}
+""")
+
+print(f"visit {node}")
+visit_order.append(node.get_value())
+print(f"""visit_order {visit_order}""")
+
+print(f"{node} has left child? {node.has_left_child()}")
+
+print(f"{node} has right child? {node.has_right_child()}")
+
+print(stack)
+
+# *** Since the dates node is a leaf node we can pop it off the stack ***
+print(stack.pop())
+
+print(stack)
+
+# now we'll set the node to the new top of the stack, which is banana
+node = stack.top()
+
+# now check banana's right child
+print(f"{node} has right child? {node.has_right_child()}")
+
+# banana doesn't have a right child, done tracking it
+# so we pop banana off the stack
+print(f"pop {stack.pop()} off stack")
+print(f"""
+stack
+{stack}
+""")
+
+# now we'll set the node to the new top of the stack, which is apple
+node = stack.top()
+
+print(node.has_right_child())
+
+if node.has_right_child():
+    node = node.get_right_child()
+    stack.push(node)
+    
+print(f"""
+visit_order {visit_order} 
+stack
+{stack}""")
+
+# visit cherry
+print(f"visit {node}")
+visit_order.append(node.get_value())
+print(f"""visit_order: {visit_order}""")
+
+# Now we'll check if cherry has a left child
+print(f"{node} has left child? {node.has_left_child()}")
+
+# it doesn't, so we'll check if it has a right child
+print(f"{node} has right child? {node.has_right_child()}")
+
+# since cherry has neither left nor right child nodes,
+# we are done tracking it, and can pop it off the stack
+
+print(f"pop {stack.pop()} off the stack")
+
+print(f"""
+visit_order {visit_order} 
+stack
+{stack}
+""")
+
+print(f"pop {stack.pop()} off stack")
+print(f"pre-order traversal visited nodes in this order: {visit_order}")
+
+class State(object):
+    def __init__(self,node):
+        self.node = node
+        self.visited_left = False
+        self.visited_right = False
+        
+    def get_node(self):
+        return self.node
+    
+    def get_visited_left(self):
+        return self.visited_left
+    
+    def get_visited_right(self):
+        return self.visited_right
+    
+    def set_visited_left(self):
+        self.visited_left = True
+        
+    def set_visited_right(self):
+        self.visited_right = True
+        
+    def __repr__(self):
+        s = f"""{self.node}
+visited_left: {self.visited_left}
+visited_right: {self.visited_right}
+        """
+        return s
+
+def pre_order_with_stack(tree, debug_mode=False):
+  visit_order = list()
+  stack = Stack()
+  node = tree.get_root()
+  visit_order.append(node.get_value())
+  state = State(node)
+  stack.push(state)
+  count = 0
+
+  while(node):
+    count += 1
+
+    if node.has_left_child() and not state.get_visited_left():
+      state.set_visited_left()
+      node = node.get_left_child()
+      visit_order.append(node.get_value())
+      state = State(node)
+      stack.push(state)
+
+    elif node.has_right_child() and not state.get_visited_right():
+      state.set_visited_right()
+      node = node.get_right_child()
+      visit_order.append(node.get_value())
+      state = State(node)
+      stack.push(state)
+    
+    else: 
+      stack.pop()
+
+      if not stack.is_empty():
+        state = stack.top()
+        node = state.get_node()
+      else: 
+        node = None
+
+    return visit_order
+
+
+      
+
+
+  
+
+
+  
+
+
