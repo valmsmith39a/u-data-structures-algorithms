5a3fc3253a609b203dccb3136c8a43b81eebb4b9
valmsmith39a <valmsmith39a@gmail.com>
2020-04-20 21:00:51 -0700

    scaffold of hash map class

--- hash_maps.py
+++ hash_maps.py
@@ -16,4 +16,16 @@ def hash_function(string):
     return hash_code
 
 
-print(hash_function('abcd'))
+class HashMap():
+
+    def __init__(self):
+        self.num_entries = 0
+
+    def put(self, key, value):
+        pass
+
+    def get(self, key):
+        pass
+
+    def size(self):
+        return self.num_entries
