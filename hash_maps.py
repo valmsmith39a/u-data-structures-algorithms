9d2558a2beb23d72b39695b0b3cb5e1141f87362
valmsmith39a <valmsmith39a@gmail.com>
2020-04-21 21:40:12 -0700

    test put, get, size

--- hash_maps.py
+++ hash_maps.py
@@ -63,15 +63,35 @@ class HashMap():
 
     def get_hash_code(self, key):
         key = str(key)
+        num_buckets = len(self.bucket_array)
         current_coefficient = 1
         hash_code = 0
 
         for character in key:
             hash_code += ord(character) * current_coefficient
+            hash_code = hash_code % num_buckets
             current_coefficient *= self.p
-            current_coefficient = current_coefficient
+            current_coefficient = current_coefficient % num_buckets
 
-        return hash_code
+        return hash_code % num_buckets
 
     def size(self):
         return self.num_entries
+
+
+hash_map = HashMap()
+
+hash_map.put("one", 1)
+hash_map.put("two", 2)
+hash_map.put("three", 3)
+hash_map.put("neo", 11)
+hash_map.put("mary", 55)
+hash_map.put("john", 59)
+
+print("size: {}".format(hash_map.size()))
+print("one: {}".format(hash_map.get("one")))
+print("neo: {}".format(hash_map.get("neo")))
+print("three: {}".format(hash_map.get("three")))
+print("john's height {}".format(hash_map.get("john")))
+print("mary's heigh {}".format(hash_map.get("mary")))
+print("size: {}".format(hash_map.size()))
